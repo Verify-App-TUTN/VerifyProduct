07534a41cd749542fe8e34b6b45d71e26ddbe266
gift56 <efeasiughu@gmail.com>
2022-07-25 13:57:19 +0100

    about styling header

--- steps/src/components/Header.jsx
+++ steps/src/components/Header.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
+import styled from 'styled-components';
+import Logo from '../assets/logo.png';
 
 const Header = () => {
     return (
-        <div>
-            Header
-        </div>
+        <Nav>
+            <div className="header">
+                <div className="brand">
+                    <img src={Logo} alt="logo" />
+                </div>
+                <ul className="headerLinks">
+                    <li>Integrate Product</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                </ul>
+            </div>
+            <button>Sign Up</button>
+        </Nav>
     )
 }
 
-export default Header
+export default Header;
+
+const Nav = styled.nav`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    .header{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+`;
