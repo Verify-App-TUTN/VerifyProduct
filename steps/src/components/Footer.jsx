3a15cd1723e7d39862ca9d270c6c382f9cbe0b0d
gift56 <efeasiughu@gmail.com>
2022-07-25 16:40:14 +0100

    system is about to off

--- steps/src/components/Footer.jsx
+++ steps/src/components/Footer.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 import Logo from '../assets/footerlg.png';
+import { FaFacebookSquare, FaLinkedin, FaInstagram, FaTwitterSquare } from 'react-icons/fa';
+
 
 const Footer = () => {
     return (
@@ -10,9 +12,53 @@ const Footer = () => {
                     <div className="leftSide">
                         <div className="brand">
                             <img src={Logo} alt="logo" />
+                            <p>
+                                <span>Get accesss to select your choice product from our list of Product</span>
+                                <span>Categories Get accesss to select your choice product from our list</span>
+                                <span> of Product Categories Get accesss</span>
+                            </p>
+                        </div>
+                        <div className="news">
+                            <p>Get Updated. Join Our Newsletter</p>
+                            <form className="subcribe">
+                                <input type="text" placeholder='Enter your email address' />
+                                <button className='btn'>Subscribe</button>
+                            </form>
                         </div>
                     </div>
-                    <div className="rightSide">Right</div>
+                    <div className="rightSide">
+                        <div className="row">
+                            <h3>Home</h3>
+                            <ul>
+                                <li>About Us</li>
+                                <li>Contact Us</li>
+                            </ul>
+                        </div>
+                        <div className="row">
+                            <h3>Services</h3>
+                            <ul>
+                                <li>Verify Products</li>
+                                <li>Report Product</li>
+                                <li>Product Category</li>
+                            </ul>
+                        </div>
+                        <div className="row">
+                            <h3>Legals</h3>
+                            <ul>
+                                <li>Terms of Use</li>
+                                <li>Privacy Policy</li>
+                            </ul>
+                        </div>
+                        <div className="row">
+                            <h3>Social Media</h3>
+                            <div className='socialIcon'>
+                                <FaLinkedin className='icon' />
+                                <FaTwitterSquare className='icon' />
+                                <FaInstagram className='icon' />
+                                <FaFacebookSquare className='icon' />
+                            </div>
+                        </div>
+                    </div>
                 </Section>
             </div>
         </div>
@@ -23,9 +69,42 @@ export default Footer;
 
 const Section = styled.footer`
      display: flex;
-     justify-content: center;
-     padding: 1rem;
+     justify-content: space-between;
+     padding: 2rem 1rem;
      .leftSide{
+        flex: 1;
+        display:  flex;
+        flex-direction: column;
+        gap: 3rem;
+        .brand{
+            display: flex;
+            flex-direction: column;
+            gap: 1rem;
+            align-items: flex-start;
+            p{
+                font-weight: 400 !important;
+                font-size: 16px;
+                line-height: 24px;
+                color: #FEFEFF;
+                span{
+                    display: block;
+                }
+            }
+        }
+        .news{
+            display: flex;
+            flex-direction: column;
+            gap: 1rem;
+            align-items: flex-start;  
+            p{
+                font-weight: 700 !important;
+                font-size: 20px;
+                line-height: 24px;
+                color: #FEFEFF;  
+            }
+        }
+     }
+     .rightSide{
         flex: 1;
      }
 `;
