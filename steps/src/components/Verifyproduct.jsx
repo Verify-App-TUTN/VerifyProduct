ec34001855747c725b6111ed0e41e2d6b077fc2e
gift56 <efeasiughu@gmail.com>
2022-07-27 13:36:03 +0100

    done with the desktop rea

--- steps/src/components/Verifyproduct.jsx
+++ steps/src/components/Verifyproduct.jsx
@@ -2,13 +2,210 @@ import React from 'react';
 import styled from 'styled-components';
 import Coke from '../assets/coke.png';
 import Rating from '../assets/rating.png';
+import Star from '../assets/Star1.png';
+import Arrow from '../assets/arrows.png';
 
 const Verifyproduct = () => {
   return (
-    <div className='container'>
-      <img src={Coke} alt="" />
-    </div>
+    <Section className='container'>
+      <div className="title">
+        <h2>Thank You for Verifying this Product.</h2>
+        <h5>This is an Original product of Company name</h5>
+      </div>
+      <div className="productDetails">
+        <h6>Product Details</h6>
+        <div className="productImages">
+          <img src={Coke} alt="Coke" />
+          <img src={Rating} alt="Details" />
+        </div>
+      </div>
+      <div className="reviewProduct">
+        <div className="leftSide">
+          <div className="reviewP">
+            <h3>Kindly Review this Product</h3>
+            <p>Give a star rating</p>
+            <div className="starsArea">
+              <img src={Star} alt="star" />
+              <img src={Star} alt="star" />
+              <img src={Star} alt="star" />
+              <img src={Star} alt="star" />
+              <img src={Star} alt="star" />
+            </div>
+          </div>
+          <form className="comments">
+            <label htmlFor="comments" className="commentsLabel">Comments</label>
+            <textarea name="comments" id="comments" cols="30" rows="10"
+              placeholder="Type your reviews here,............"></textarea>
+            <button className="btn">Submit</button>
+          </form>
+          <div className="moreP">
+            <h4>Verify Another Product?</h4>
+            <button className="btn">Verify Now</button>
+          </div>
+        </div>
+        <div className="rightSide">
+          <img src={Arrow} alt="" />
+        </div>
+      </div>
+    </Section>
   )
 }
 
-export default Verifyproduct
+export default Verifyproduct;
+
+const Section = styled.section`
+          .title {
+              display: flex;
+              flex-direction: column;
+              text-align: center;
+              margin-bottom: 10px;
+            h2 {
+              font-size: 60px;
+              font-style: normal;
+              font-weight: 900;
+              color: #000D09;
+              margin-top: 20px;
+             }
+              h5 {
+              font-size: 35px;
+              font-style: normal;
+              font-weight: 700;
+              color: #000D09;
+              opacity: 0.5;
+            }
+          }
+          .productDetails {
+              display: flex;
+              flex-direction: column;
+              text-align: center;
+              justify-content: center;
+              align-items: center;
+              gap: 1.5rem;
+              margin-bottom: 20px;
+              h6 {
+                font-size: 25px;
+                font-weight: 900;
+                color: #000D09;
+              }
+             .productImages {
+              display: flex;
+              justify-content: center;
+              align-items: flex-start;
+              gap: 4rem;
+              img:first-child {
+               width: 485px;
+               height: 408px;
+              }
+              img:last-child {
+                width: 590px;
+               height: 336px;
+              }
+            }
+        }
+            .reviewProduct {
+              display: flex;
+              justify-content: center;
+              position: relative;
+              .leftSide {
+                flex: 1;
+                padding: 1.5rem;
+                padding-left: 4rem !important;
+              .reviewP {
+                display: flex;
+                flex-direction: column;
+                gap: 10px;
+                justify-content: center;
+                align-items: flex-start;
+                margin-bottom: 2rem;
+              h3 {
+                font-size: 30px;
+                font-weight: 700;
+                color: #000D09;
+              }
+              p {
+                font-size: 20px;
+                font-size: 400;
+                color: #009F78;
+              }
+              .starsArea {
+                display: flex;
+                gap: 8px;
+               img {
+                 width: 40px;
+                 height: 40px;
+                 cursor: pointer;
+                 transition: all 300ms ease;
+                 &:hover {
+                  transform: scale(1.5);
+                 }
+                }
+              }
+            }
+              .comments {
+                display: flex;
+                flex-direction: column;
+                justify-content: flex-start;
+                align-items: flex-start;
+                gap: 10px;
+                margin-bottom: 3rem;
+             .commentsLabel {
+                font-size: 18px;
+                font-weight: 700;
+                color: #000D09;
+              }
+              #comments {
+                width: 85%;
+                height: 136px;
+                padding: 16px;
+                background: rgba(90, 224, 191, 0.2);
+                outline: none;
+                border: 1px solid #b5e6dd;
+                border-radius: 8px;
+                opacity: 0.5;
+                font-size: 14px;
+                font-weight: 400;
+                color: #000D09 !important;
+               &::placeholder {
+                 color: #000d09;
+                 font-size: 14px;
+                 font-weight: 400;
+                }
+               &:focus {
+                 border: 2px solid #009F78;
+               }
+              }
+              button{
+               background: #009F78;
+               border-color: #009F78;
+              }
+            }
+              .moreP {
+                display: flex;
+                flex-direction: column;
+                align-items: flex-start;
+                gap: 5px;
+               h4 {
+                font-size: 48px;
+                font-weight: 400;
+                color: #000d09;
+               }
+               button{
+                background: #241818;
+                border-color: #241818;
+               }
+              }
+            }
+              .rightSide {
+                flex: 1;
+              overflow: hidden;
+              img {
+                position: absolute;
+              left: 600px;
+              top: -290px;
+              height: 52rem;
+              z-index: -1;
+              max-width: 100%;
+             }
+            }
+        }
+              `;
