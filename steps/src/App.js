d539a8fe0610a4d5c107cb483a9d871646a28929
gift56 <efeasiughu@gmail.com>
2022-08-06 15:00:31 +0100

    fixing lizzy code

--- steps/src/App.js
+++ steps/src/App.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import './App.css';
-import ProductN from './components/ProductN';
-import Footer from './components/Footer';
-import VerifyForm from './components/VerifyForm';
+import React from "react";
+import "./App.css";
+import ProductN from "./components/ProductN";
+import Footer from "./components/Footer";
+import VerifyForm from "./components/VerifyForm";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Verifyproduct from './components/Verifyproduct';
-import Landing from './components/Landing';
+import Verifyproduct from "./components/Verifyproduct";
+import Landing from "./components/Landing";
 
 function App() {
-  document.title = "Verify Product"
+  document.title = "Verify Product";
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route index path='/' element={<ProductN />} />
-          <Route path='verifyform' element={<VerifyForm />} />
-          <Route path='verifyproduct' element={<Verifyproduct />} />
-          <Route path='landingpage' element={<Landing />} />
+          <Route index path="/" element={<Landing />} />
+          <Route path="verifyform" element={<VerifyForm />} />
+          <Route path="verifyproduct" element={<Verifyproduct />} />
+          <Route path="product" element={<ProductN />} />
         </Routes>
       </BrowserRouter>
       <Footer />
