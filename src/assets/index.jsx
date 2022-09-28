af1adbe03bb8d454ab7812385aa4115549ff1efd
Udoka <udochukwukaonyela@gmail.com>
2022-09-28 23:45:29 +0100

    fixes

--- src/assets/index.jsx
+++ src/assets/index.jsx
@@ -2,4 +2,5 @@ export { default as ArrowWatermark } from './illustrations/ArrowWatermark';
 export { default as Authenticity } from './illustrations/Authenticity';
 export { default as Growth } from './illustrations/Growth';
 export { default as HeroIllustration } from './illustrations/HeroIllustration';
+export { default as Logo } from './icons/Logo';
 export { default as Reliability } from './illustrations/Reliability';
