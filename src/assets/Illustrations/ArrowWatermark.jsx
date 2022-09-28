f1a5ab9fc96e70d4363a89a09e5a479f36f2136d
Udoka <udochukwukaonyela@gmail.com>
2022-09-28 23:06:28 +0100

    fixes

--- src/assets/Illustrations/ArrowWatermark.jsx
+++ src/assets/Illustrations/ArrowWatermark.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import style from "./index.module.css";
 
 const ArrowWatermark = (props) => {
   return (
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,8 +1,16 @@
 import React from "react";
+import { HeroIllustration } from "../../assets";
 import style from "./index.module.css";
 
 const Hero = () => {
-  return <div className={style.Hero}>Hero</div>;
+  return <div className={style.Hero}>
+    <div className={style.Hero__content}>
+      <h1 className={style.Hero__title}>Integrate Your Products Into Our Database</h1>
+      <p className={style.Hero__subtitle}>Get accesss to select your choice product from our list of Product Categories Get accesss to select your choice product from our list of Product Categories Get accesss to</p>
+      <button className={style.Hero__button}>Get Started</button>
+    </div>
+    <HeroIllustration />
+  </div>;
 }
 
 export default Hero;
