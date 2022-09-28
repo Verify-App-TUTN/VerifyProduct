402a48932381e5e0fc8aeaf599ba8f9ccb11f1e8
Udoka <udochukwukaonyela@gmail.com>
2022-09-28 22:49:42 +0100

    fixes

--- src/assets/Illustrations/ArrowWatermark.jsx
+++ src/assets/Illustrations/ArrowWatermark.jsx
@@ -3,7 +3,9 @@ import style from "./index.module.css";
 
 const ArrowWatermark = (props) => {
   return (
-    <svg {...props} width="833" height="946" viewBox="0 0 833 946" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg
+      {...props}
+      width="833" height="946" viewBox="0 0 833 946" fill="none" xmlns="http://www.w3.org/2000/svg">
     <g opacity="0.7">
       <rect x="35.6235" y="667.133" width="85.9384" height="257.815" rx="42.9692" transform="rotate(-47.3722 35.6235 667.133)" fill="#5AE0BF" fill-opacity="0.2" />
       <rect x="773.457" y="449.203" width="85.9384" height="608.444" rx="42.9692" transform="rotate(56.525 773.457 449.203)" fill="#5AE0BF" fill-opacity="0.2" />
