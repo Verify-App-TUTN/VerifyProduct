6a23c953286123f2ddb037fe7821799c06cfb42c
Udoka <udochukwukaonyela@gmail.com>
2022-09-28 22:20:42 +0100

    added export file

---
+++ src/pages/index.jsx
@@ -0,0 +1,4 @@
+export { default as VerifyForm } from './VerifyForm';
+export { default as Verifyproduct } from './VerifyProduct/Verifyproduct';
+export { default as ProductN } from './Product/ProductN';
+export { default as Home } from './Home/Home';
