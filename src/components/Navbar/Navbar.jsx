180049d13b40b1395d6c0661f562792ab876b6c9
Udoka <udochukwukaonyela@gmail.com>
2022-09-28 23:45:42 +0100

    added links

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,46 @@
 import React from "react";
 import style from "./index.module.css";
-
+import { NavLink as Link } from "react-router-dom";
+import { NavLink } from "../";
+import { Logo } from "../../assets";
+const navLinks = [
+  {
+    text: "Integrate Product",
+    to: "/integrateproduct",
+  },
+  {
+    text: "About Us",
+    to: "/aboutus",
+  },
+  {
+    text: "Contact Us",
+    to: "/contactus",
+  },
+]
 const Navbar = () => {
-  return <div className={style.Navbar}>Navbar</div>;
+  return (
+    <nav className={style.Navbar}>
+      <div className={style.Navbar__logo}>
+        <Link to="/">
+          <Logo />
+        </Link>
+      </div>
+      <ul className={style.Navbar__links}>
+        {navLinks.map(({ text, to }) => (
+          <li key={text}>
+          <NavLink to={to}>     
+            {text}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+      <div className={style.Navbar__button}>
+        <NavLink to="/signup" >
+          Get Started
+        </NavLink>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
