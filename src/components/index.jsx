d8484327847ebc98f464f61b147421ffe9b956ed
Udoka <udochukwukaonyela@gmail.com>
2022-09-28 21:51:58 +0100

    added fonts

---
+++ src/components/index.jsx
@@ -0,0 +1,8 @@
+export { default as About } from './About/About';
+export { default as CTA } from './CTA/CTA';
+export { default as Features } from './Features/Features';
+export { default as Footer } from './Footer/Footer';
+export { default as Hero } from './Hero/Hero';
+export { default as Layout } from './Layout/Layout';
+export { default as Navbar } from './Navbar/Navbar';
+export { default as Navlink } from './Navlink/Navlink';
