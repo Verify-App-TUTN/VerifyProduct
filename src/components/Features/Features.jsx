88bb4c13669c8f88d88352bdee813aee92f997f5
Udoka <udochukwukaonyela@gmail.com>
2022-09-28 22:10:44 +0100

    created features component

---
+++ src/components/Features/Features.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import style from "./index.module.css";
+
+const Features = () => {
+  return <div className={style.Features}>Features</div>;
+}
+
+export default Features;
