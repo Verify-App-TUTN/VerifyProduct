c0e5c7fa4f0c9585c64decf8f2a97b75d6a5b21e
Udoka <udochukwukaonyela@gmail.com>
2022-09-29 12:13:27 +0100

    created component for each feature card

---
+++ src/components/FeatureCard/FeatureCard.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import style from "./index.module.css";
+
+const FeatureCard = ({ icon, heading, description }) => {
+  return <div className={style.FeatureCard}>FeatureCard</div>;
+}
+
+export default FeatureCard;
