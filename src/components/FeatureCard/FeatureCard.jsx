4a57b58cb43764e5b236aed5e2e0decb6eea9562
Udoka <udochukwukaonyela@gmail.com>
2022-09-29 12:29:38 +0100

    fixed icon component render

--- src/components/FeatureCard/FeatureCard.jsx
+++ src/components/FeatureCard/FeatureCard.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import style from "./index.module.css";
 
-const FeatureCard = ({ children, heading, description }) => {
+const FeatureCard = ({ children: Children, heading, description }) => {
   return (
     <div className={style.FeatureCard}>
-      <div className={style.FeatureCard__icon}>{children}</div>
+      <div className={style.FeatureCard__icon}>{<Children />}</div>
       <div className={style.FeatureCard__heading}>{heading}</div>
       <div className={style.FeatureCard__description}>{description}</div>
     </div>
