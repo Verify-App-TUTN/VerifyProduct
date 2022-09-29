aafd9c3dbb7141d368971217af06c1c4f40135a2
Udoka <udochukwukaonyela@gmail.com>
2022-09-29 14:47:02 +0100

    fixes

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,18 +1,17 @@
 import React from "react";
-import { AboutImage } from "../../assets";
 import GetStarted from "../GetStarted/GetStarted";
 import style from "./index.module.css";
-const Details = () => {
+const Details = ({ reversed, image, title, description }) => {
   return (
-    <div className={style.Details}>
-      <img src={AboutImage} className={style.Details__image} alt="description" />
+    <div className={`${style.Details} ${reversed && style.Details__reverse}`}>
+      <img src={image} className={style.Details__image} alt={title} />
       <div className={style.Details__content}>
-        <h3 className={style.Details__content__title}>We offer a close range monitoring system.</h3>
-        <p className={style.Details__content__description}>Report Fake Products You’ve Come Across With Report Fake Products You’ve Come Across With Report Fake Products You’ve Come Across With</p>
+        <h3 className={style.Details__content__title}>{title}</h3>
+        <p className={style.Details__content__description}>{description}</p>
         <GetStarted className={style.Details__content__button} />
       </div>
     </div>
   );
-}
+};
 
 export default Details;
