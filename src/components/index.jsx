aac0427535d2ef90964be7237a488fd4adee028a
Udoka <udochukwukaonyela@gmail.com>
2022-09-29 12:13:41 +0100

    exported components

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -7,3 +7,5 @@ export { default as Layout } from './Layout/Layout';
 export { default as Navbar } from './Navbar/Navbar';
 export { default as NavLink } from './NavLink/NavLink';
 export { default as GetStarted } from './GetStarted/GetStarted';
+export { default as FeatureCard } from './FeatureCard/FeatureCard';
+export { default as FeatureCards } from './FeatureCards/FeatureCards';
