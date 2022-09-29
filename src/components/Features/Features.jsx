68c152e15d948658ae7b56b02a6c633858df41db
Udoka <udochukwukaonyela@gmail.com>
2022-09-29 12:15:10 +0100

    added markup

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { GreenLogo } from "../../assets";
 import style from "./index.module.css";
 
+
 const Features = () => {
+
   return (
     <div className={style.Features}>
-      <h1></h1>
+      <span className={style.Features__heading}><h3>Why</h3> <GreenLogo /> <h3>?</h3></span>
+      <div className={style.Features__cards}>
+
+      </div>
     </div>
   );
 }
