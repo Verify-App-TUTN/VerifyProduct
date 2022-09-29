3d04b935c454b135c1ca9b9a1195bc9cce8e0176
Udoka <udochukwukaonyela@gmail.com>
2022-09-29 22:09:15 +0100

    exported icons

--- src/assets/index.jsx
+++ src/assets/index.jsx
@@ -6,7 +6,9 @@ export { default as Growth } from './illustrations/Growth';
 export { default as GreenLogo } from './icons/GreenLogo';
 export { default as HeroIllustration } from './illustrations/HeroIllustration';
 export { default as Logo } from './icons/Logo';
-// export Facebook icons
-
+export { default as Instagram } from './icons/Instagram';
+export { default as Twitter } from './icons/Twitter';
+export { default as LinkedIn } from './icons/LinkedIn';
+export { default as FaceBook } from './icons/FaceBook';
 export { default as Reliability } from './illustrations/Reliability';
 export { SystemsImage, AccessibilityImage };
