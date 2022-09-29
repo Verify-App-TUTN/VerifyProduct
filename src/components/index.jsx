1f63cd766ee930de321a76aa39b61e254ce939a9
Udoka <udochukwukaonyela@gmail.com>
2022-09-29 14:46:42 +0100

    exported component

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,11 +1,12 @@
 
 export { default as CTA } from './CTA/CTA';
+export { default as Details } from './Details/Details';
 export { default as Features } from './Features/Features';
+export { default as FeatureCard } from './FeatureCard/FeatureCard';
+export { default as FeatureCards } from './FeatureCards/FeatureCards';
 export { default as Footer } from './Footer/Footer';
+export { default as GetStarted } from './GetStarted/GetStarted';
 export { default as Hero } from './Hero/Hero';
 export { default as Layout } from './Layout/Layout';
 export { default as Navbar } from './Navbar/Navbar';
 export { default as NavLink } from './NavLink/NavLink';
-export { default as GetStarted } from './GetStarted/GetStarted';
-export { default as FeatureCard } from './FeatureCard/FeatureCard';
-export { default as FeatureCards } from './FeatureCards/FeatureCards';
