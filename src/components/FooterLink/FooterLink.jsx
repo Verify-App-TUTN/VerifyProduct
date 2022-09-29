897fcce977b231492481e0ac93d7932e51f7c584
Udoka <udochukwukaonyela@gmail.com>
2022-09-29 21:30:56 +0100

    fixes

--- src/components/FooterLink/FooterLink.jsx
+++ src/components/FooterLink/FooterLink.jsx
@@ -1,13 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import style from "./index.module.css";
 
-const FooterLink = () => {
+const FooterLink = ({ heading, links }) => {
+  
   return (
     <div className={style.FooterLink}>
-      <h1 className={style.FooterLink__heading}>Home</h1>
+      <h1 className={style.FooterLink__heading}>{ heading }</h1>
       <div className={style.FooterLink__links}>
-          <span>About Us</span>
-          <span>Contact US</span>
+        {links.map(({ text, to }) => (
+          <div className={style.FooterLink__links__link} key={text}>
+            <Link to={to}>{text}</Link>
+          </div>
+        ))}
       </div>
     </div>
   );
