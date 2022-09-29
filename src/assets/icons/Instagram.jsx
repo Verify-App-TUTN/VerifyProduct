d795e262701952cdacada6715730c2bd3be105cb
Udoka <udochukwukaonyela@gmail.com>
2022-09-29 22:20:49 +0100

    fixed scgs

--- src/assets/icons/Instagram.jsx
+++ src/assets/icons/Instagram.jsx
@@ -12,6 +12,7 @@ const Instagram = (props) => {
     >
       <path d="M23,0H9C4,0,0,4,0,9v6v8c0,5,4,9,9,9h14c5,0,9-4,9-9v-8V9C32,4,28,0,23,0z" />
       <circle fill="#fff" cx="16" cy="16" r="8" />
+      <circle fill="#000" cx="16" cy="16" r="4" />
       <circle fill="#fff" cx="25" cy="6" r="2" />
     </svg>
   );
