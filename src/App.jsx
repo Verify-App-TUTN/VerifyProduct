519434aed0ff4d01b437fe86ff764451f958a4d6
Udoka <udochukwukaonyela@gmail.com>
2022-09-29 10:32:00 +0100

    changed imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import "./App.css";
+import "./index.css";
 import {Home, ProductN, VerifyForm, Verifyproduct} from "./pages"
 import { Route, Routes } from "react-router-dom";
 function App() {
   document.title = "Verify Product";
   return (
     <Routes>
-      <Route index path="/" element={<Home />} />
+      <Route index path="/:ff" element={<Home />} />
       {/* <Route path="/verifyform" element={<VerifyForm />} />
       <Route path="/verifyproduct" element={<Verifyproduct />} />
       <Route path="/product" element={<ProductN />} /> */}
