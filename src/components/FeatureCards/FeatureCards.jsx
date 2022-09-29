fd4f4977a87a6fde94703609df1f4ae76fdb7fcc
Udoka <udochukwukaonyela@gmail.com>
2022-09-29 12:14:08 +0100

    created feature card

---
+++ src/components/FeatureCards/FeatureCards.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Authenticity, Growth, Reliability } from "../../assets";
+import FeatureCard from "../FeatureCard/FeatureCard";
+import style from "./index.module.css";
+const featuresCards = [
+  {
+    icon: Authenticity,
+    heading: "Authenticity",
+    description: "Get access to select your choice product from our list of Product Categories Get accesss to select your ",
+  },{
+    icon: Growth,
+    heading: "Product Growth",
+    description: "Get access to select your choice product from our list of Product Categories Get accesss to select your ",
+  },{
+    icon: Reliability,
+    heading: "Reliability",
+    description: "Get access to select your choice product from our list of Product Categories Get accesss to select your ",
+  },
+]
+const FeatureCards = () => {
+  return (<div className={style.FeatureCards}>
+    {featuresCards.map((Details) => (<FeatureCard />))}
+    
+  </div>);
+}
+
+export default FeatureCards;
