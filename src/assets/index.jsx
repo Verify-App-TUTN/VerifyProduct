b74730dcb0545118d781a767669b38d7268caf8a
Udoka <udochukwukaonyela@gmail.com>
2022-09-29 12:13:33 +0100

    exported new icons

--- src/assets/index.jsx
+++ src/assets/index.jsx
@@ -1,6 +1,7 @@
 export { default as ArrowWatermark } from './illustrations/ArrowWatermark';
 export { default as Authenticity } from './illustrations/Authenticity';
 export { default as Growth } from './illustrations/Growth';
+export { default as GreenLogo } from './icons/GreenLogo';
 export { default as HeroIllustration } from './illustrations/HeroIllustration';
 export { default as Logo } from './icons/Logo';
 export { default as Reliability } from './illustrations/Reliability';
