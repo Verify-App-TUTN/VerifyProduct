d39352abca8866d1999b5c954e7d334c37ed42f5
Udoka <udochukwukaonyela@gmail.com>
2022-09-29 11:51:34 +0100

    adding texts

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import style from "./index.module.css";
 
 const Features = () => {
-  return <div className={style.Features}>Features</div>;
+  return (
+    <div className={style.Features}>
+      <h1></h1>
+    </div>
+  );
 }
 
 export default Features;
