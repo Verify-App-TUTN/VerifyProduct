759b1130bdd624ba1a4cde073fcd833fa5634be1
Udoka <udochukwukaonyela@gmail.com>
2022-09-29 10:33:58 +0100

    created get started button

---
+++ src/components/GetStarted/GetStarted.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import style from "./index.module.css";
+
+const GetStarted = ({className, ...props}) => {
+  return(
+    <Link to="getStarted">
+    <span {...props} className={style.GetStarted + " "+ className}
+        
+      >
+        Get Started
+      </span>
+    </Link>
+  );
+}
+
+export default GetStarted;
