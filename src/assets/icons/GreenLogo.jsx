36a3395e0e7123f7ba4cec5d49ba59469fc73dc4
Udoka <udochukwukaonyela@gmail.com>
2022-09-29 11:51:24 +0100

    created logo component

---
+++ src/assets/icons/GreenLogo.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import style from "./index.module.css";
+
+const GreenLogo = () => {
+  return <div className={style.GreenLogo}>GreenLogo</div>;
+}
+
+export default GreenLogo;
