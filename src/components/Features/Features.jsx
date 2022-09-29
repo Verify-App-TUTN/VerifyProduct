0fdab3247ffabf7e37ba2c1d43a18cc0086d1d51
Udoka <udochukwukaonyela@gmail.com>
2022-09-29 23:07:00 +0100

    adding classNames

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -8,7 +8,7 @@ const Features = () => {
 
   return (
     <div className={style.Features}>
-      <div className={style.Features__heading}><h3>Why</h3> <GreenLogo /> <h3>?</h3></div>
+      <div className={style.Features__heading}><h3>Why</h3> <GreenLogo className={style.Features__heading__logo} /> <h3>?</h3></div>
       <FeatureCards />
     </div>
   );
