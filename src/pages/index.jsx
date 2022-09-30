2d039ce58718ffb57c339bd1229c52a810d8645d
Udoka <udochukwukaonyela@gmail.com>
2022-09-30 11:40:31 +0100

    exports

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,3 +3,4 @@ export { default as Verifyproduct } from './VerifyProduct/Verifyproduct';
 export { default as ProductN } from './Product/ProductN';
 export { default as Home } from './Home/Home';
 export { default as ComingSoon } from './ComingSoon/ComingSoon';
+export {default as ContactUs } from './ContactUs/ContactUs';
