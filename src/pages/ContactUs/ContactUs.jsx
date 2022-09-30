4f8cf362a0112ce215f79e9ed7b5d63b4b4c33f3
Udoka <udochukwukaonyela@gmail.com>
2022-09-30 15:59:02 +0100

    fixes

--- src/pages/ContactUs/ContactUs.jsx
+++ src/pages/ContactUs/ContactUs.jsx
@@ -6,10 +6,12 @@ const ContactUs = () => {
   return (
     <Layout className={style.ContactUs}>
       <div className={style.ContactUs__content}>
-        <h1 className={style.ContactUs__content__heading}>Integrate Your Products Into Our Database</h1>
+        <div className={style.ContactUs__forms}>
+          <h1 className={style.ContactUs__content__heading}>Integrate Your Products Into Our Database</h1>
+        </div>
         
+        <img src="image" alt="Contact Us" className={style.ContactUs__content__image} />
       </div>
-
     </Layout>
   )
     ;
