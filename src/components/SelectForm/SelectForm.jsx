39a852efa71d6e05af703632e8591430f4df73f5
Udoka <udochukwukaonyela@gmail.com>
2022-09-30 20:28:00 +0100

    for select forms

---
+++ src/components/SelectForm/SelectForm.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import style from "./index.module.css";
+
+const SelectForm = () => {
+  return <div className={style.SelectForm}>SelectForm</div>;
+}
+
+export default SelectForm;
