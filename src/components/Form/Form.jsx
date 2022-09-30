601b806867629556a5279b7dcabf864190517e2c
Udoka <udochukwukaonyela@gmail.com>
2022-09-30 19:58:28 +0100

    added classNames

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -3,8 +3,9 @@ import style from "./index.module.css";
 
 const Form = ({ errorMessage, label, placeholder }) => {
   return (
-    <div style={style.Form}>
-        <label></label>
+    <div className={style.Form}>
+      <label className={style.Form__label}>Company/Group Name</label>
+      <input type="text" placeholder="Input Country/Group Name" className={style.Form__input} />
     </div>
   );
 }
