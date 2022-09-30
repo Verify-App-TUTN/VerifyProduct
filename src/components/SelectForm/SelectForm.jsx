f3843364c36cdecc3d85830d473cf47175d1aaf4
Udoka <udochukwukaonyela@gmail.com>
2022-09-30 21:49:13 +0100

    added form details

--- src/components/SelectForm/SelectForm.jsx
+++ src/components/SelectForm/SelectForm.jsx
@@ -1,8 +1,24 @@
 import React from "react";
+import { Arrow } from "../../assets";
+import { FormDetail } from "../";
 import style from "./index.module.css";
 
 const SelectForm = () => {
-  return <div className={style.SelectForm}>SelectForm</div>;
-}
+  const details = ["United States", "Nigeria", "Croatia", "Germany"];
+  return (
+    <div className={style.Form}>
+      <label className={style.Form__label}>Company/Group Name</label>
+      <div className={style.Form__input}>
+        Select Industry
+        <Arrow />
+      </div>
+      <div className={style.Form__details}>
+        {details.map((details) => (
+          <FormDetail {...{details}} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default SelectForm;
