b3a06018df55c2b1880b78f6f965effcdac1363f
Udoka <udochukwukaonyela@gmail.com>
2022-09-30 22:53:56 +0100

    added logic for form props

--- src/components/TextForm/TextForm.jsx
+++ src/components/TextForm/TextForm.jsx
@@ -1,13 +1,13 @@
-import React from "react";
-import { Arrow } from "../../assets";
+import React, { useEffect } from "react";
 import style from "./index.module.css";
 
-const TextForm = ({ errorMessage, label, placeholder }) => {
-  
+const TextForm = ({ errorMessage, label, placeholder, state, type, setState }) => {
+
   return (
     <div className={style.Form}>
       <label className={style.Form__label}>Company/Group Name</label>
-      <input type="text" placeholder="Input Country/Group Name" className={style.Form__input} />
+      <input type={type} value={state} onChange={setState} placeholder="Input Country/Group Name" className={style.Form__input} />
+      <p className={style.Form__error}>{errorMessage}</p>
     </div>
   );
 }
