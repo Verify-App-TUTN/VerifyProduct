8029cc741347f6b03187c00815fafb0cb66488f4
Udoka <udochukwukaonyela@gmail.com>
2022-09-30 10:41:54 +0100

    created coming soon page

---
+++ src/pages/ComingSoon/ComingSoon.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import style from "./index.module.css";
+
+const ComingSoon = () => {
+  return <section className={style.ComingSoon}>ComingSoon</section>;
+}
+
+export default ComingSoon;
