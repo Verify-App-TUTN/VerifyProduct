d7a7f377f5775d23c73c6a9c282e7e9c7148934b
Udoka <udochukwukaonyela@gmail.com>
2022-09-30 20:16:53 +0100

    fixes

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -5,7 +5,10 @@ const Form = ({ errorMessage, label, placeholder }) => {
   return (
     <div className={style.Form}>
       <label className={style.Form__label}>Company/Group Name</label>
-      <input type="text" placeholder="Input Country/Group Name" className={style.Form__input} />
+      {/* <input type="text" placeholder="Input Country/Group Name" className={style.Form__input} /> */}
+      <div className={`${style.Form__select__input} ${style.Form__input}`}>
+        Select Industry
+      </div>
     </div>
   );
 }
