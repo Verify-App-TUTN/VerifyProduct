58b099a30912c47e7a78debf30dd168eb38468ec
Udoka <udochukwukaonyela@gmail.com>
2022-09-30 23:05:06 +0100

    fixes

--- src/utils/hooks/useTextForm.jsx
+++ src/utils/hooks/useTextForm.jsx
@@ -1,12 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useId } from "react";
 import { TextForm } from "../../components";
 import { useInput } from "./";
 
 const useForm = ({placeholder,label, errorMessage,type}) => {
   const [state, setState] = useInput();
+  const id = useId();
   return [state, () => {
     return (
-              <TextForm {...{placeholder, state, setState, errorMessage, label, type}} />
+      <TextForm {...{ placeholder, state, setState, errorMessage, label, type, key: label.trim() + id }} />
+      
             )
   }];
 }
