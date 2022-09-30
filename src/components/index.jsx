3c2d5a7199460baaf5199111fab0052e61bc40a9
Udoka <udochukwukaonyela@gmail.com>
2022-09-30 19:26:23 +0100

    exports

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -5,6 +5,7 @@ export { default as Features } from './Features/Features';
 export { default as FeatureCard } from './FeatureCard/FeatureCard';
 export { default as FeatureCards } from './FeatureCards/FeatureCards';
 export { default as Footer } from './Footer/Footer';
+export { default as Form } from "./Form/Form";
 export { default as GetStarted } from './GetStarted/GetStarted';
 export { default as Hero } from './Hero/Hero';
 export { default as Layout } from './Layout/Layout';
