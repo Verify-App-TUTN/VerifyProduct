b519c9c48b83e714e9f6689310855992f164d9bd
Udoka <udochukwukaonyela@gmail.com>
2022-09-30 11:40:09 +0100

    added contactus route

---
+++ src/pages/ContactUs/ContactUs.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Layout } from "../../components";
+import style from "./index.module.css";
+
+const ContactUs = () => {
+  return (
+    <Layout className={style.ContactUs}>
+      <div className={style.ContactUs__content}>
+        <h1 className={style.ContactUs__content__heading}>Integrate Your Products Into Our Database</h1>
+        
+      </div>
+
+    </Layout>
+  )
+    ;
+}
+
+export default ContactUs;
