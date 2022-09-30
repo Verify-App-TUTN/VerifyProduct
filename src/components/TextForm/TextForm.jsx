f54bd93d0ab3149a88118b0be3113a2ee1aff431
Udoka <udochukwukaonyela@gmail.com>
2022-09-30 21:49:21 +0100

    added text form

--- src/components/TextForm/TextForm.jsx
+++ src/components/TextForm/TextForm.jsx
@@ -7,11 +7,7 @@ const TextForm = ({ errorMessage, label, placeholder }) => {
   return (
     <div className={style.Form}>
       <label className={style.Form__label}>Company/Group Name</label>
-      {/* <input type="text" placeholder="Input Country/Group Name" className={style.Form__input} /> */}
-      <div className={`${style.Form__select__input} ${style.Form__input}`}>
-        Select Industry
-        <Arrow />
-      </div>
+      <input type="text" placeholder="Input Country/Group Name" className={style.Form__input} />
     </div>
   );
 }
