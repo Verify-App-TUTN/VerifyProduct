38f11e38d1f6ad6a78eb4f023781b33819ac1d90
Udoka <udochukwukaonyela@gmail.com>
2022-09-30 23:38:28 +0100

    fixes

--- src/components/FormDetail/FormDetail.jsx
+++ src/components/FormDetail/FormDetail.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from "react";
 import style from "./index.module.css";
 
-const FormDetail = ({ details, setDetail, selected }) => {
+const FormDetail = ({ details, setState, state }) => {
   const [isSelected, setIsSelected] = useState(false);
   useEffect(() => {
-    setIsSelected(selected === details);
-  }, [selected, details]);
-  return <div className={`${style.FormDetail} ${isSelected && style.FormDetail__active}`}>{details}</div>;
+    setIsSelected(state === details);
+  }, [state, details]);
+  return <div
+    onClick={() => setState(details)}
+    className={`${style.FormDetail} ${isSelected && style.FormDetail__active}`}>{details}</div>;
 }
 
 export default FormDetail;
