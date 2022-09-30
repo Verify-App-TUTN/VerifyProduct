3b9f6e7547359283d090b0e44e487b991c86d2d4
Udoka <udochukwukaonyela@gmail.com>
2022-09-30 19:17:22 +0100

    created form component for form inputs

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "./index.module.css";
+
+const Form = ({ errorMessage, label, placeholder }) => {
+  return (
+    <div style={style.Form}>
+        <label></label>
+    </div>
+  );
+}
+
+export default Form;
