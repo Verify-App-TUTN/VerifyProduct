f15b84b0b81536f6ccf74cee6443f45316d972ea
Udoka <udochukwukaonyela@gmail.com>
2022-09-30 11:40:25 +0100

    added contact us route

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "./index.css";
-import {ComingSoon, Home} from "./pages"
+import {ComingSoon, ContactUs, Home} from "./pages"
 import { Route, Routes } from "react-router-dom";
 const  App = () => {
   document.title = "Verify Product";
   return (
     <Routes>
       <Route index path="/" element={<Home />} />
+      <Route index path="/contactus" element={<ContactUs />} />
       <Route index path="/:page" element={<ComingSoon />} />
       {/* <Route path="/verifyform" element={<VerifyForm />} />
       <Route path="/verifyproduct" element={<Verifyproduct />} />
