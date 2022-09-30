c414165ff47823468121c37409a66b3fa45def65
Udoka <udochukwukaonyela@gmail.com>
2022-09-30 11:31:23 +0100

    fixes

--- src/pages/ComingSoon/ComingSoon.jsx
+++ src/pages/ComingSoon/ComingSoon.jsx
@@ -7,8 +7,8 @@ const ComingSoon = () => {
   return (
     <Layout className={style.ComingSoon}>
       <div className={style.ComingSoon__content}>
-        <img src={ComingSoonIllustration} alt="Coming Soon Illustration" className={style.ComingSoon__illustration} />
         <h1 className={style.ComingSoon__content__heading}>Coming Soon</h1>
+        <img src={ComingSoonIllustration} alt="Coming Soon Illustration" className={style.ComingSoon__illustration} />
       </div>
     </Layout>
   );
