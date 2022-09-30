748da4fbf6af20f44acf4ca942faf850fd8bcef5
Udoka <udochukwukaonyela@gmail.com>
2022-09-30 11:02:52 +0100

    added coming soon page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "./index.css";
-import {Home} from "./pages"
+import {ComingSoon, Home} from "./pages"
 import { Route, Routes } from "react-router-dom";
 const  App = () => {
   document.title = "Verify Product";
   return (
     <Routes>
       <Route index path="/" element={<Home />} />
+      <Route index path="/:page" element={<ComingSoon />} />
       {/* <Route path="/verifyform" element={<VerifyForm />} />
       <Route path="/verifyproduct" element={<Verifyproduct />} />
       <Route path="/product" element={<ProductN />} /> */}
