b49b017044bf5145f7ae5e318dac4b159480450d
Udoka <udochukwukaonyela@gmail.com>
2022-09-30 20:27:16 +0100

    created forms

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -5,6 +5,8 @@ export { default as Features } from './Features/Features';
 export { default as FeatureCard } from './FeatureCard/FeatureCard';
 export { default as FeatureCards } from './FeatureCards/FeatureCards';
 export { default as Footer } from './Footer/Footer';
+export { default as TextForm } from "./TextForm/TextForm";
+export { default as SelectForm } from "./SelectForm/SelectForm";
 export { default as Form } from "./Form/Form";
 export { default as GetStarted } from './GetStarted/GetStarted';
 export { default as Hero } from './Hero/Hero';
