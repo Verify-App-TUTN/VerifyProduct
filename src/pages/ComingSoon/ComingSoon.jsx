537e1757e4b1143cc536634ce87bbf06987e670f
Udoka <udochukwukaonyela@gmail.com>
2022-09-30 11:21:53 +0100

    fixes

--- src/pages/ComingSoon/ComingSoon.jsx
+++ src/pages/ComingSoon/ComingSoon.jsx
@@ -1,8 +1,17 @@
 import React from "react";
+import { ComingSoonIllustration } from "../../assets";
+import { Layout } from "../../components";
 import style from "./index.module.css";
 
 const ComingSoon = () => {
-  return <section className={style.ComingSoon}>ComingSoon</section>;
+  return (
+    <Layout className={style.ComingSoon}>
+      <div className={style.ComingSoon__content}>
+        <img src={ComingSoonIllustration} alt="Coming Soon Illustration" className={style.ComingSoon__illustration} />
+        <h1 className={style.ComingSoon__content__heading}>Coming Soon</h1>
+      </div>
+    </Layout>
+  );
 }
 
 export default ComingSoon;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,3 +2,4 @@ export { default as VerifyForm } from './VerifyForm';
 export { default as Verifyproduct } from './VerifyProduct/Verifyproduct';
 export { default as ProductN } from './Product/ProductN';
 export { default as Home } from './Home/Home';
+export { default as ComingSoon } from './ComingSoon/ComingSoon';
