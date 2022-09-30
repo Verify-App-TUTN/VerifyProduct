ff684a091921b2aed1ff95047ea4ce9e3e2b2205
Udoka <udochukwukaonyela@gmail.com>
2022-09-30 22:38:50 +0100

    added selected logic

--- src/components/FormDetail/FormDetail.jsx
+++ src/components/FormDetail/FormDetail.jsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import style from "./index.module.css";
 
-const FormDetail = ({details, setDetail}) => {
-  return <div className={style.FormDetail}>{details}</div>;
+const FormDetail = ({ details, setDetail, selected }) => {
+  const [isSelected, setIsSelected] = useState(false);
+  useEffect(() => {
+    setIsSelected(selected === details);
+  }, [selected, details]);
+  return <div className={`${style.FormDetail} ${isSelected && style.FormDetail__active}`}>{details}</div>;
 }
 
 export default FormDetail;
