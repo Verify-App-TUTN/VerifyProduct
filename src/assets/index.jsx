40ec269993325e2e24e84ccc56d01b3b5bdad7e3
Udoka <udochukwukaonyela@gmail.com>
2022-09-30 20:27:08 +0100

    exported new forms

--- src/assets/index.jsx
+++ src/assets/index.jsx
@@ -6,6 +6,7 @@ export { default as ArrowWatermark } from './illustrations/ArrowWatermark';
 export { default as Authenticity } from './illustrations/Authenticity';
 export { default as Growth } from './illustrations/Growth';
 export { default as GreenLogo } from './icons/GreenLogo';
+export { default as Arrow } from './icons/Arrow';
 export { default as HeroIllustration } from './illustrations/HeroIllustration';
 export { default as Logo } from './icons/Logo';
 export { default as Instagram } from './icons/Instagram';
