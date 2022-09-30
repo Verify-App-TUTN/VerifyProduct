519f2ea7218025c683a80bb0b3690517f13c8a6d
Udoka <udochukwukaonyela@gmail.com>
2022-09-30 23:04:49 +0100

    added forms

--- src/pages/ContactUs/ContactUs.jsx
+++ src/pages/ContactUs/ContactUs.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { ContactUsImage } from "../../assets";
-import { Form, Layout, SelectForm, TextForm } from "../../components";
+import {  Layout, SelectForm, TextForm } from "../../components";
+import { useTextForm } from "../../utils/hooks";
 import style from "./index.module.css";
 
 const ContactUs = () => {
+  const [companyName, CompanyNameInput] = useTextForm({ placeholder: "Input Country/Group Name", label: "Company/Group Name", errorMessage: "Input Required", type: "text" });
+  const [Email, EmailInput] = useTextForm({ placeholder: "Input Email", label: "Email", errorMessage: "Input Required", type: "email" });
   return (
     <Layout className={style.ContactUs}>
       <div className={style.ContactUs__content}>
@@ -11,7 +14,8 @@ const ContactUs = () => {
         <div className={style.ContactUs__content__forms}>
           <h1 className={style.ContactUs__content__forms__heading}>Integrate Your Products Into Our Database</h1>
           <div className={style.ContactUs__content__forms__container}>
-            <TextForm />
+            <CompanyNameInput />
+            <EmailInput />
             <SelectForm />
           </div>
         </div>
