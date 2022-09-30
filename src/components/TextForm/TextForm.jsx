3db66e24460363fec365c5075bfd077a094100b5
Udoka <udochukwukaonyela@gmail.com>
2022-09-30 20:54:46 +0100

    fixes

---
+++ src/components/TextForm/TextForm.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Arrow } from "../../assets";
+import style from "./index.module.css";
+
+const TextForm = ({ errorMessage, label, placeholder }) => {
+  
+  return (
+    <div className={style.Form}>
+      <label className={style.Form__label}>Company/Group Name</label>
+      {/* <input type="text" placeholder="Input Country/Group Name" className={style.Form__input} /> */}
+      <div className={`${style.Form__select__input} ${style.Form__input}`}>
+        Select Industry
+        <Arrow />
+      </div>
+    </div>
+  );
+}
+
+export default TextForm;
