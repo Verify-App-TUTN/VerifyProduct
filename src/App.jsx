f31586568b648ed2f87940a0507c62dd39486d4d
Udoka <udochukwukaonyela@gmail.com>
2022-09-30 12:53:18 +0100

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./index.css";
-import {ComingSoon, ContactUs, Home, ProductN, VerifyForm, Verifyproduct} from "./pages"
+import {ComingSoon,  Home, ProductN, VerifyForm, Verifyproduct} from "./pages"
 import { Route, Routes } from "react-router-dom";
 const  App = () => {
   document.title = "Verify Product";
