7e9c2c855ec5d77e0103ef8eb5386816439c8e7d
Udoka <udochukwukaonyela@gmail.com>
2022-10-30 23:08:22 +0100

    fixes

---
+++ src/components/FormDetail/FormDetail.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from "react";
+import style from "./index.module.css";
+
+const FormDetail = ({ details, setState, state, close }) => {
+  const [isSelected, setIsSelected] = useState(false);
+  useEffect(() => {
+    setIsSelected(state === details);
+  }, [state, details]);
+  return <div
+    onClick={() => {
+      setState(details);
+      close();
+    }}
+    className={`${style.FormDetail} ${isSelected && style.FormDetail__active}`}>{details}</div>;
+}
+
+export default FormDetail;
