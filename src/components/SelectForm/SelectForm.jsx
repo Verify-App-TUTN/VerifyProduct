6f4aa97c58efd0d9edb369bf29291bc1c6c707c3
Udoka <udochukwukaonyela@gmail.com>
2022-11-03 22:01:31 +0100

    added error message

--- src/components/SelectForm/SelectForm.jsx
+++ src/components/SelectForm/SelectForm.jsx
@@ -1,27 +1,20 @@
-import React, { useEffect, useRef, useState } from "react";
-import { Arrow } from "../../assets";
-import { FormDetail } from "../";
+import React from "react";
 import style from "./index.module.css";
 
-const SelectForm = ({ setState, state, details, label, errorMessage, placeholder }) => {
-  const [toggleDetails, setToggleDetails] = useState(false);
-  console.log(toggleDetails)
-  const [detailsHeight, setDetailsHeight] = useState(0);
-  const detailsContainerRef  = useRef(null);
-  console.log(detailsHeight);
+const SelectForm = ({ setState, state, details, label, errorMessage, placeholder, error }) => {
   return (
     <div className={style.Form}>
       <label className={style.Form__label}>{label}</label>
       <select
-        // className={style.Form__details}
         className={style.Form__input}
         onChange={(e) => setState(e.target.value)}
-        // value={state}
+        value={state}
       >
         {details?.map((details) => (
           <option value={details}>{details === "" ? placeholder : details  }</option>
         ))}
       </select>
+      {error && <p className={style.Form__error}>{errorMessage}</p>}
     </div>
   );
 };
