6695d4474d6e2a6c9e8e2c0eaf8db0b1b4561386
Udoka <udochukwukaonyela@gmail.com>
2022-11-03 22:01:29 +0100

    added error message

--- src/components/TextForm/TextForm.jsx
+++ src/components/TextForm/TextForm.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import style from "./index.module.css";
 
-const TextForm = ({ errorMessage, label, placeholder, state, type, setState }) => {
+const TextForm = ({ errorMessage, label, placeholder, state, type, setState, error }) => {
 
   return (
     <div className={style.Form}>
       <label className={style.Form__label}>{ label }</label>
       <input type={type} value={state} onChange={setState} placeholder={placeholder} className={style.Form__input} />
-      {/* <p className={style.Form__error}>{errorMessage}</p> */}
+      {error && <p className={style.Form__error}>{errorMessage}</p>}
     </div>
   );
 }
