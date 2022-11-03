52b6b31554a2e5ec679dec9654c94d4f432baceb
Udoka <udochukwukaonyela@gmail.com>
2022-11-03 21:37:35 +0100

    added tags and elements

--- src/components/PopUp/PopUp.jsx
+++ src/components/PopUp/PopUp.jsx
@@ -1,8 +1,16 @@
 import React from "react";
+import Success from "../../assets/icons/Success";
 import style from "./index.module.css";
 
-const PopUp = () => {
-  return <div className={style.PopUp}>PopUp</div>;
-}
+const PopUp = ({active}) => {
+  return (
+    <main className={`${style.PopUp} ${active ? style.show : style.hide}`}>
+      <div className={style.PopUp__container}>
+        <Success className={style.PopUp__container__icon}/>
+        <p>Data Submitted successfully</p>
+      </div>
+    </main>
+  );
+};
 
 export default PopUp;
